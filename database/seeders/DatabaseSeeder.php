aff35a19a0b0b6b78ca97d0e7eb46c69637b1b60
Heaven Aldrico <4d3.h34v3n@gmail.com>
2025-10-07 11:53:29 +0700

    fix: production admin panel access

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,6 +17,12 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]);
+
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@igsindonesia.org',
+            'password' => Hash::make('.Srimanik123.'),
         ]);
 
         $this->call([
